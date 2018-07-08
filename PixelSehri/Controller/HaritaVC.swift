74e85945e943e001faac894b610b9d91eedef876
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-07-08 21:44:27 +0300

    Proje Oluşturuldu ve Gerekli Podlar Eklendi

---
+++ PixelSehri/Controller/HaritaVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PixelSehri
+//
+//  Created by Ahmed Selim Üzüm on 6.07.2018.
+//  Copyright © 2018 Ahmed Selim Üzüm. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class HaritaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
