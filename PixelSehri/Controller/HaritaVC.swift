ea5748c5026540a9a1916a9d1fc4f36c8090da81
Ahmed Selim Üzüm <ahmedselimuzum@ahmedselimuzum.local>
2018-07-09 00:07:57 +0300

    Haritaya Lokasyon Bilgileri Girildi Ders-99

--- PixelSehri/Controller/HaritaVC.swift
+++ PixelSehri/Controller/HaritaVC.swift
@@ -7,23 +7,53 @@
 //
 
 import UIKit
-import Alamofire
+import CoreLocation
 import MapKit
 
 class HaritaVC: UIViewController {
 
     @IBOutlet weak var haritaGoruntuleyicisi: MKMapView!
+    var lokasyonDuzenleyicisi=CLLocationManager();
+    let yetkiDurumu=CLLocationManager.authorizationStatus();
+    let bolgeRadyus:Double=1000;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        lokacsyonServisleriAyarla();
+        lokasyonDuzenleyicisi.delegate=self;
         haritaGoruntuleyicisi.delegate=self;
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func haritayiOrtalaBasildi(_ sender: Any) {
+        if yetkiDurumu == .authorizedAlways || yetkiDurumu == .authorizedWhenInUse{
+            haritayiLokasyonaGoreAyarla();
+        }
     }
     
 }
 
 extension HaritaVC: MKMapViewDelegate{
+    func haritayiLokasyonaGoreAyarla(){
+        guard let koordinat=lokasyonDuzenleyicisi.location?.coordinate else{return;}
+        let koordinatBolgesi=MKCoordinateRegion(center: koordinat, latitudinalMeters: bolgeRadyus*2, longitudinalMeters: bolgeRadyus*2)
+        haritaGoruntuleyicisi.setRegion(koordinatBolgesi, animated: true);
+    }
+}
+
+extension HaritaVC: CLLocationManagerDelegate{
+    
+    func lokacsyonServisleriAyarla(){
+        if yetkiDurumu == .notDetermined{
+            lokasyonDuzenleyicisi.requestAlwaysAuthorization();
+        }
+        else{
+            return;
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        haritayiLokasyonaGoreAyarla();
+    }
     
 }
